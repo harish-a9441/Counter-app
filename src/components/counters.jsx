eb8ca8d17ef87d9eb9512616d5d48fe90e8e0c13
Harish Alavarapu <harish.allavarapu@gmail.com>
2024-03-17 19:23:58 +0530

    first update

---
+++ src/components/counters.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class Counters extends Component {
+    state = {  
+        counters:[
+            {id:1,value:0},
+            {id:1,value:0},
+            {id:1,value:0},
+            {id:1,value:0},
+        ]
+    }; 
+    render() { 
+        return (
+            <div>
+                {this.state.counters.map(counter => <Counter key={Counter.id} />)}
+        </div>);
+    }
+}
+ 
+export default Counters;
